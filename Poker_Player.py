a21fbb6e3745fb1a232f03b30e9b7c73f9cbbf13
Mike Teplitsky <mikiteplitsky@gmail.com>
2021-08-11 22:54:29 +0300

    Update 11.08.21

--- Poker_Player.py
+++ Poker_Player.py
@@ -7,17 +7,18 @@
 from Deck import *
 from constants import *
 
-class Poker_Player:
-# credit need to change to the minimum buy-in of the table or the amount the player want 'till max table buy-in
 
-    def __init__(self, nick ,buy_in):
+class Poker_Player:
+    # credit need to change to the minimum buy-in of the table or the amount the player want 'till max table buy-in
+
+    def __init__(self, nick, buy_in):
         self.credit = buy_in
         self.hand = list()
         self.name = nick
-    
+
     def reset_hand(self):
         hand = list()
-        
+
     def get_card(self, deck):
         self.hand.extend(deck.deal_card())
 
